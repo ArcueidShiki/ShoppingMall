d0ebac3592a8483e86f1fa10ebbf13445b6432fa
Administrator <1726627514@qq.com>
2022-05-24 20:30:40 +0800

    向睿

---
+++ productController/src/test/java/cn/tedu/productcontroller/ProductControllerApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.tedu.productcontroller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductControllerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
