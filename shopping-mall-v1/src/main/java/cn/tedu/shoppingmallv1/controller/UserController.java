bfdcafb2df05239af1acfbf26eb2fb9aaa12427e
狐臭 <1063673132@qq.com>
2022-05-25 11:00:09 +0800

    stash

--- shopping-mall-v1/src/main/java/cn/tedu/shoppingmallv1/controller/UserController.java
+++ shopping-mall-v1/src/main/java/cn/tedu/shoppingmallv1/controller/UserController.java
@@ -1,8 +1,5 @@
 package cn.tedu.shoppingmallv1.controller;
 
-import cn.tedu.shoppingmallv1.mapper.UserMapper;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserController {
