13433364f76c64fa2d6d035f3d5ff19238013145
Arcueid <451676498@qq.com>
2022-05-25 11:05:56 +0800

    彭 - 同步远程更新

--- shopping-mall-v1/src/main/java/cn/tedu/shoppingmallv1/controller/UploadController.java
+++ shopping-mall-v1/src/main/java/cn/tedu/shoppingmallv1/controller/UploadController.java
@@ -1,6 +1,5 @@
 package cn.tedu.shoppingmallv1.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
--- shopping-mall-v1/src/main/java/cn/tedu/shoppingmallv1/controller/UserController.java
+++ shopping-mall-v1/src/main/java/cn/tedu/shoppingmallv1/controller/UserController.java
@@ -1,6 +1,10 @@
 package cn.tedu.shoppingmallv1.controller;
 
 
+import cn.tedu.shoppingmallv1.mapper.UserMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
 @RestController
 public class UserController {
     @Autowired(required = false)
