a7ea304e17aee7660defef12e086811b9f738414
狐臭 <1063673132@qq.com>
2022-05-25 12:40:23 +0800

    xcy-完成基本注册登录功能

--- shopping-mall-v1/src/main/java/cn/tedu/shoppingmallv1/controller/UserController.java
+++ shopping-mall-v1/src/main/java/cn/tedu/shoppingmallv1/controller/UserController.java
@@ -9,4 +9,26 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
     @Autowired(required = false)
     UserMapper mapper;
+
+    @RequestMapping("/login")
+    public int login(@RequestBody User user){
+        User u = mapper.selectByName(user.getUsername());
+        if(u!=null){
+            if(u.getPassword().equals(user.getPassword())){
+                return 1;//登录成功
+            }
+            return 2;//密码错误
+        }
+        return 3;//用户名不存在
+    }
+
+    @RequestMapping("/reg")
+    public int reg(@RequestBody User user){
+        User u = mapper.selectByName(user.getUsername());
+        if(u != null){
+         return 2;//用户名被占用
+        }
+        mapper.insert(user);
+        return 1;
+    }
 }
