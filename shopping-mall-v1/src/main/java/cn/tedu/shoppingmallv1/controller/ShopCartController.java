844877dc05ee5b04ac5ab35ef18924847cfdb1df
DSELLO <1363720061@qq.com>
2022-05-30 23:42:10 +0800

    DSELLO 更改

--- shopping-mall-v1/src/main/java/cn/tedu/shoppingmallv1/controller/ShopCartController.java
+++ shopping-mall-v1/src/main/java/cn/tedu/shoppingmallv1/controller/ShopCartController.java
@@ -1,11 +1,30 @@
 package cn.tedu.shoppingmallv1.controller;
 
+import cn.tedu.shoppingmallv1.entity.Product;
 import cn.tedu.shoppingmallv1.mapper.ShopCartMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class ShopCartController {
     @Autowired(required = false)
     ShopCartMapper mapper;
+
+    @RequestMapping("/user/shopCart/select")
+    public List<Product> select(){
+        return mapper.select();
+    }
+
+    @RequestMapping("/user/shopCart/insert")
+    public String insert(){
+        return mapper.insert();
+    }
+
+    @RequestMapping("/user/shopCart/update")
+    public String update(){
+        return mapper.update();
+    }
 }
